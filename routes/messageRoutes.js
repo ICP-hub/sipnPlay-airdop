58c796f68e02b88d3890496f7fec0309737d18ff
somiyaratna <beherasomiyaratna@gmail.com>
2024-10-22 17:29:02 +0530

    Approved Admins and Principal Encryptions

--- routes/messageRoutes.js
+++ routes/messageRoutes.js
@@ -1,8 +1,12 @@
-const express = require('express');
-const { sendMessage, getMessages } = require('../controllers/messageController');
+const express = require("express");
+const {
+  sendMessage,
+  getMessages,
+} = require("../controllers/messageController");
+const authenticatePrincipal = require("../middlewares/authMiddleware");
 const router = express.Router();
 
-router.post('/sendMessage', sendMessage);
-router.get('/getMessages', getMessages);
+router.post("/sendMessage", sendMessage);
+router.get("/getMessages", authenticatePrincipal, getMessages);
 
 module.exports = router;
--- routes/waitlistRoutes.js
+++ routes/waitlistRoutes.js
@@ -1,8 +1,12 @@
-const express = require('express');
-const { sendWaitlist, getWaitlist } = require('../controllers/waitlistController');
+const express = require("express");
+const {
+  sendWaitlist,
+  getWaitlist,
+} = require("../controllers/waitlistController");
+const authenticatePrincipal = require("../middlewares/authMiddleware");
 const router = express.Router();
 
-router.post('/sendWaitlist', sendWaitlist);
-router.get('/getWaitlist', getWaitlist);
+router.post("/sendWaitlist", sendWaitlist);
+router.get("/getWaitlist", authenticatePrincipal, getWaitlist);
 
 module.exports = router;
